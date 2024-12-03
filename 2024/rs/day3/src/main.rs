d0dfa4a9d698eb98d6e7fcef218e44f9358f3fba
cuckflong <johnathanclong13@gmail.com>
2024-12-03 17:24:02 +0800

    day3 better

--- 2024/rs/day3/src/main.rs
+++ 2024/rs/day3/src/main.rs
@@ -30,41 +30,23 @@ fn part_one(input: &str) {
 }
 
 fn part_two(input: &str) {
-    let mults_re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
-    let mults: Vec<_> = mults_re.find_iter(input).collect();
-
-    let dos_re = Regex::new(r"do\(\)").unwrap();
-    let dos: Vec<_> = dos_re.find_iter(input).collect();
-    let dos_starts: Vec<usize> = dos.iter().map(|d| d.start()).collect();
-
-    let donts_re = Regex::new(r"don't\(\)").unwrap();
-    let donts: Vec<_> = donts_re.find_iter(input).collect();
-    let donts_starts: Vec<usize> = donts.iter().map(|d| d.start()).collect();
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)").unwrap();
+    let matches: Vec<_> = re.find_iter(input).collect();
 
     let mut enabled = true;
-    let mut last_mult_start = 0;
     let mut sum = 0;
-    for mult in mults {
-        let mult_start = mult.start();
-        while last_mult_start < mult_start {
+
+    for match_ in matches {
+        if match_.as_str() == "do()" {
+            enabled = true;
+        } else if match_.as_str() == "don't()" {
+            enabled = false;
+        } else {
             if enabled {
-                for dont_start in &donts_starts {
-                    if dont_start <= &mult_start && dont_start > &last_mult_start {
-                        enabled = false;
-                    }
-                }
-            } else {
-                for do_start in &dos_starts {
-                    if do_start <= &mult_start && do_start > &last_mult_start {
-                        enabled = true;
-                    }
-                }
+                sum += parse_mul_strings(&match_.as_str().to_string());
             }
-            last_mult_start += 1;
-        }
-        if enabled {
-            sum += parse_mul_strings(&mult.as_str().to_string());
         }
     }
+
     println!("{}", sum);
 }
